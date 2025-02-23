a3554bc12ec42f9c2677c61d5011778717dec5a7
i-douk <101892232+i-douk@users.noreply.github.com>
2025-02-23 22:35:19 +0000

    revert migration and start over bucket policy bc upload unexpectadly failed again

--- frontend/supabase/migrations/20250223103053_..sql
+++
@@ -1,5 +0,0 @@
--- Allow authenticated users to insert (upload) files
-create policy "Allow authenticated users to upload files"
-on storage.objects
-for insert
-with check ( auth.role() = 'authenticated' );
--- frontend/supabase/migrations/20250223133459_..sql
+++
@@ -1,8 +0,0 @@
-CREATE POLICY "Allow authenticated users to delete their own files in public folder"
-ON storage.objects
-FOR DELETE USING (
-  bucket_id = 'avatar_images'
-  AND (storage.foldername(name))[1] = 'public'
-  AND auth.role() = 'authenticated'
-  AND auth.uid() = owner
-);
