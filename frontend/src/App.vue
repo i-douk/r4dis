e1c49730b12709aa3990e3aa257d847c074807e4
i-douk <101892232+i-douk@users.noreply.github.com>
2025-03-02 11:16:30 +0000

    edit suspens funct with component following tuto

--- frontend/src/App.vue
+++ frontend/src/App.vue
@@ -8,17 +8,25 @@ onMounted(() => {
   authStore.trackPodcasterAuthChanges()
   usePageStore()
 })
+
+
 </script>
 <template>
   <UserLayout>
     <Toaster />
-    <Suspense>
+    <RouterView v-slot="{Component, route}" >
+    <Suspense v-if="Component" :timeout="0">
       <template #default>
-        <RouterView />
+          <Component  :is="Component" :key="route.name"/>
       </template>
       <template #fallback>
+        <div
+        class="absolute top-1/2 transform -translate-y-1/2 left-1/2 -translate-x-1/2 flex justify-center items-center w-full h-screen bg-background bg-opacity-90 z-50"
+        >
         <iconify-icon icon="lucide:loader-circle"></iconify-icon>
-      </template>
-    </Suspense>
+      </div>
+    </template>
+  </Suspense>
+</RouterView>
   </UserLayout>
 </template>
