78bcf9e795c6cc4bd43e822f947df90f6051fd18
i-douk <101892232+i-douk@users.noreply.github.com>
2025-04-03 13:15:54 +0000

    start ws implementation - broken

--- backend/index.ts
+++ backend/index.ts
@@ -46,5 +46,20 @@ sequelize
     console.error("Unable to sync database:", error);
   });
 
+import WebSocket, { WebSocketServer } from 'ws';
+
+const wss = new WebSocketServer( { port: 8088 });
+  
+wss.on('connection', function connection(ws) {
+  ws.on('error', console.error);
+  
+  ws.on('message', function message(data, isBinary) {
+    wss.clients.forEach(function each(client) {
+      if (client !== ws && client.readyState === WebSocket.OPEN) {
+        client.send(data, { binary: isBinary });
+      }
+    });
+  });
+});
 
 export default app;
--- frontend/src/main.ts
+++ frontend/src/main.ts
@@ -10,3 +10,24 @@ const app = createApp(App)
 app.use(createPinia())
 app.use(router)
 app.mount('#app')
+
+import WebSocket from 'isomorphic-ws';
+
+const ws = new WebSocket('wss://localhost:8088');
+
+ws.onopen = function open() {
+  console.log('connected');
+  ws.send(Date.now());
+};
+
+ws.onclose = function close() {
+  console.log('disconnected');
+};
+
+ws.onmessage = function incoming(data) {
+  console.log(`Roundtrip time: ${Date.now() - data.data} ms`);
+
+  setTimeout(function timeout() {
+    ws.send(Date.now());
+  }, 500);
+};
